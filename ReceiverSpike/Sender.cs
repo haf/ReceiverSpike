76e76559cc16a5f55d120e4e03b9ef8445f43495
Henrik <henrik@haf.se>
2011-12-05 23:35:32 +0100

    initial commit

---
+++ ReceiverSpike/Sender.cs
@@ -0,0 +1,337 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedMember.Global
+
+using System;
+using System.Collections.Generic;
+using System.Concurrency;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using Stact;
+using It = Machine.Specifications.It;
+using Scheduler = System.Concurrency.Scheduler;
+
+namespace ReceiverSpike
+{
+	public interface Context : IEquatable<Context>, IComparable<Context>
+	{
+		/// <summary>
+		/// Gets the message id/request id that casued the event.
+		/// </summary>
+		Guid MessageId { get; }
+
+		/// <summary>
+		/// Gets the message object.
+		/// </summary>
+		Event Message { get; }
+	}
+
+	public interface Event
+	{
+		/// <summary>
+		/// Gets the aggregate root id
+		/// </summary>
+		Guid Id { get; }
+
+		/// <summary>
+		/// Gets the event version
+		/// </summary>
+		ulong Version { get; }
+		
+		/// <summary>
+		/// Gets the full type name.
+		/// </summary>
+		string Type { get; }
+	}
+
+	public interface IsSortable : IEquatable<IsSortable>, IComparable<IsSortable>
+	{
+		Event Value { get; }
+	}
+
+	interface InsertEvent
+	{
+		Event Event { get; }
+	}
+
+
+	internal class InsertEventCmd : InsertEvent
+	{
+		readonly Event _event;
+
+		public InsertEventCmd(Event @event)
+		{
+			Contract.Requires(@event != null);
+			_event = @event;
+		}
+
+		public Event Event
+		{
+			get { return _event; }
+		}
+	}
+
+	#region Sample events
+
+	internal class MsgA : Event
+	{
+		public MsgA(Guid id, ulong version)
+		{
+			Id = id;
+			Version = version;
+		}
+
+		public Guid Id { get; protected set; }
+		public ulong Version { get; protected set; }
+
+		public string Type
+		{
+			get { return this.Type(); }
+		}
+	}
+
+	internal class MsgB : Event
+	{
+		public MsgB(Guid id, ulong version)
+		{
+			Id = id;
+			Version = version;
+		}
+
+		public Guid Id { get; protected set; }
+		public ulong Version { get; protected set; }
+
+		public string Type
+		{
+			get { return this.Type(); }
+		}
+	}
+
+	internal class MsgC
+		: Event
+	{
+		public MsgC(Guid id, ulong version)
+		{
+			Id = id;
+			Version = version;
+		}
+
+		public Guid Id { get; protected set; }
+		public ulong Version { get; protected set; }
+
+		public string Type
+		{
+			get { return this.Type(); }
+		}
+	}
+
+	#endregion
+
+	#region future
+
+	public interface IPersistable
+	{
+	}
+
+	public static class PersistableExtensions
+	{
+		public static void Persist(this IPersistable persistable)
+		{
+		}
+	}
+
+	#endregion
+
+
+	public interface QueryInternals
+	{
+	}
+
+	public interface EventHeapState
+	{
+		ulong MaxAcceptedItem { get; }
+		ulong MinPendingItem { get; }
+	}
+
+	class EventHeapStateImpl : EventHeapState
+	{
+		ulong _mai, _mpi;
+		public EventHeapStateImpl(ulong mai, ulong mpi) { _mai = mai; _mpi = mpi; }
+		ulong EventHeapState.MaxAcceptedItem { get { return _mai; } }
+		ulong EventHeapState.MinPendingItem { get { return _mpi; } }
+	}
+
+	public interface EventHeapActor<T> : ActorRef, IObservable<T>
+	{
+		void Insert(T @event);
+	}
+
+	#region Example Consumers
+
+	/// <summary>
+	/// Interface for consumers to implement, when they want to specify what messages they want to consume.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	internal interface Consume<in T>
+	{
+		void Consume(T message);
+	}
+
+	/// <summary>
+	/// Consume ONLY MsgB
+	/// </summary>
+	internal class FinickyConsumer
+		: Consume<MsgB>
+	{
+		public void Consume(MsgB message)
+		{
+		}
+	}
+
+	/// <summary>
+	/// A unfastidious consumer that listens to a great many events.
+	/// </summary>
+	internal class UnfastidiousConsumer
+		: Consume<MsgA>, Consume<MsgB>, Consume<MsgC>
+	{
+		void Consume<MsgA>.Consume(MsgA message)
+		{
+		}
+
+		void Consume<MsgB>.Consume(MsgB message)
+		{
+		}
+
+		void Consume<MsgC>.Consume(MsgC message)
+		{
+		}
+	}
+
+	#endregion
+
+	/// <summary>
+	/// care about MsgB
+	/// </summary>
+	public abstract class finicky_consumer_context
+	{
+		protected static TestScheduler testScheduler;
+		protected static Guid arId;
+		protected static ActorRef eventHeap;
+		protected static EventHeap eventHeapImpl;
+
+		protected static IEnumerable<IsSortable> returned;
+
+		Establish context_where_we_care_about_msgb = () =>
+			{
+				testScheduler = new TestScheduler();
+				arId = Guid.NewGuid();
+				// care about MsgB
+				eventHeap = ActorFactory.Create(inbox => (eventHeapImpl = new EventHeap(typeof(FinickyConsumer), inbox, testScheduler))).GetActor();
+			};
+
+		/// <summary>
+		/// after calling this method, the actor is dead
+		/// </summary>
+		/// <param name="action"></param>
+		protected static void when_inserting(Action<EventHeap> action)
+		{
+			var store = new ListObservable<Event>(eventHeapImpl);
+
+			using (eventHeapImpl)
+				action(eventHeapImpl);
+
+			testScheduler.Run();
+
+			returned = store.Select(x => x.Sortable());
+		}
+	}
+
+	[Subject(typeof(EventHeap), "the subscribed values returned from the event heap")]
+	public class when_inserting_multiple_events_they_are_filtered_spec 
+		: finicky_consumer_context
+	{
+		Because I_am_adding_two_nodes_of_different_types = () => when_inserting(heap =>
+			{
+				heap.Insert(new MsgB(arId, 1L).Sortable());
+				heap.Insert(new MsgA(arId, 1L).Sortable());
+			});
+
+		It should_only_contain_one_item = () => returned.Count().ShouldEqual(1);
+		It should_only_contain_a_message_b = () => returned.ShouldContainOnly(new MsgB(arId, 1L).Sortable());
+	}
+
+	public abstract class heap_internal_state_context
+		: finicky_consumer_context
+	{
+		protected static Future<EventHeapState> reply;
+
+		Establish context = () => reply = new Future<EventHeapState>();
+
+		Because I_query_internal_state = () =>
+			{
+				AnonymousActor.New(inbox => eventHeap.Request<QueryInternals>(inbox).Receive<EventHeapState>(msg => reply.Complete(msg)));
+				reply.WaitUntilCompleted(-1);
+			};
+	}
+
+	[Subject("the event heap")]
+	public class initial_status_of_event_heap_spec
+		: heap_internal_state_context
+	{
+		Establish that_I_dont_add_any_messages = () => when_inserting(_ => { });
+		It should_reply_with_event_heap_state_message = () => reply.Value.ShouldNotBeNull();
+		It should_have_default_max_accepted_item_mai = () => reply.Value.MaxAcceptedItem.ShouldEqual(0UL);
+	}
+
+	[Subject(typeof(EventHeap))]
+	public class status_of_event_heap_after_adding_similar_messages_spec
+		: heap_internal_state_context
+	{
+		Establish I_have_three_messages_of_type_b_added = () => when_inserting(into_heap =>
+			{
+				Action<Event> i = e => into_heap.Insert(e.Sortable());
+				i(new MsgA(arId, 1UL));
+				i(new MsgC(arId, 2UL));
+				i(new MsgB(arId, 3UL)); // B
+				i(new MsgB(arId, 4UL)); // B
+				i(new MsgA(arId, 5UL));
+				i(new MsgB(arId, 6UL)); // B
+			});
+
+		It should_only_reply_three = 
+			() => returned.Count().ShouldEqual(3);
+
+		It should_have_max_accepted_item_mai =
+			() => reply.Value.MaxAcceptedItem.ShouldEqual(6UL);
+
+		It should_have_returned_three_msg_b = 
+			() => returned.ShouldContain(new MsgB(arId, 3UL), new MsgB(arId, 4UL), new MsgB(arId, 6UL));
+	}
+
+
+	[Subject(typeof(EventHeap))]
+	public class status_of_event_heap_with_gap
+		: heap_internal_state_context
+	{
+		Establish context_where_a_gap_of_received_messages_exists = () =>
+			{
+				Action<Event> i = e => eventHeapImpl.Insert(e.Sortable());
+				i(new MsgA(arId, 1UL));
+				// no version=2
+				// no version=3
+				i(new MsgB(arId, 4UL)); // B
+				i(new MsgA(arId, 5UL));
+				i(new MsgB(arId, 6UL)); // B
+			};
+
+		It should_have_a_max_accepted_item_mai =
+			() => reply.Value.MaxAcceptedItem.ShouldEqual(1UL);
+
+		It should_have_a_min_pending_item =
+			() => reply.Value.MinPendingItem.ShouldEqual(4UL);
+	}
+
+}
---
+++ ReceiverSpike/sortable_specifications.cs
@@ -0,0 +1,73 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedMember.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+
+using System;
+using Machine.Specifications;
+
+namespace ReceiverSpike
+{
+	// passing
+	[Subject(typeof(IsSortable))]
+	public class equality_spec
+	{
+		static Guid someArId = Guid.NewGuid();
+		static Guid anotherArId = Guid.NewGuid();
+
+		static IsSortable type_b_sortable;
+		static IsSortable type_a_sortable;
+
+		Establish context = () =>
+		{
+			type_a_sortable = new MsgA(anotherArId, 1UL).Sortable();
+			type_b_sortable = GenerateBSortable();
+		};
+
+		static IsSortable GenerateBSortable()
+		{
+			return new MsgB(someArId, 1UL).Sortable();
+		}
+
+		It should_not_equal =
+			() => type_b_sortable.Equals(type_a_sortable).ShouldBeFalse();
+
+		It should_equal_when_same_ref =
+			() => type_b_sortable.Equals(type_b_sortable).ShouldBeTrue();
+
+		It should_equal_when_not_same_ref_same_data =
+			() => type_b_sortable.Equals(GenerateBSortable()).ShouldBeTrue();
+
+		It should_not_equal_when_different_version =
+			() => type_b_sortable.Equals(new MsgB(someArId, 2UL).Sortable());
+
+		It should_not_equal_when_different_ar_id =
+			() => type_b_sortable.Equals(new MsgB(anotherArId, 1UL).Sortable());
+	}
+
+	// passing
+	[Subject(typeof(IsSortable))]
+	public class order_spec
+	{
+		static Guid arId;
+		static IsSortable version_one_sortable;
+		static IsSortable version_two_sortable;
+
+		Establish context = () =>
+		{
+			arId = Guid.NewGuid();
+			version_one_sortable = new MsgA(arId, 1UL).Sortable();
+			version_two_sortable = new MsgA(arId, 2UL).Sortable();
+		};
+
+		It should_compare_to_greater =
+			() => version_two_sortable.CompareTo(version_one_sortable).ShouldEqual(1);
+
+		It should_compare_to_lesser =
+			() => version_one_sortable.CompareTo(version_two_sortable).ShouldEqual(-1);
+
+		It should_compare_to_equal =
+			() => version_one_sortable.CompareTo(version_one_sortable).ShouldEqual(0);
+	}
+}
